33d729462f2576db74953a956a4c0a86bf9eb01e
Thomas Bolinger <bolitj01@pfw.edu>
2023-11-13 12:52:37 -0500

    Set up stack exercise

--- exercises/tdd_stack/stack.mjs
+++ exercises/tdd_stack/stack.mjs
@@ -1,19 +1,7 @@
+/**
+ * Handles a stack that behaves according to
+ * the LIFO (Last In, First Out) principle.
+ */
 class Stack {
-    constructor(){
-        this.top = -1;
-        this.data = [];
 
-    }
-    push(data){
-        this.data = [data, ...this.data];
-        this.top = this.data[0];
-    }
-    pop(){
-        this.data = this.data.slice(1, this.data.length);
-        let oldTop = this.top;
-        this.top = this.data[0];
-        return oldTop;
-    }
 }
-
-export default Stack;
